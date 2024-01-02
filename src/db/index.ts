371434fde552d1a8367d76144ce04e10f5496101
Muhsin-42 <muhsinofficial21@gmail.com>
2024-01-02 22:59:00 +0530

    Migrated To typescript

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
+import { DB_NAME } from "../constants";
 
 const connectToDB = async () => {
+  console.log("DBNAME ", process.env.PORT);
   try {
     await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
     console.log("CONNECTED TO DB");
